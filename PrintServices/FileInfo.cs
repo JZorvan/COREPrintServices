b1551c35b96b7a8e47c9886dea26a669991561a0
JZorvan <jzorvan@gmail.com>
2017-10-09 12:16:19 -0500

    Added list of two sided docs

---
+++ PrintServices/FileInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintServices
+{
+    class FileInfo
+    {
+        public static List<string> TwoSidedFiles = new List<string>()
+        {
+            "C:/PrintServices\\lp32-15C.pdf",
+            "C:/PrintServices\\lp59-1401.pdf",
+            "C:/PrintServices\\lp59-1503.pdf",
+            "C:/PrintServices\\lp59-1801.pdf",
+            "C:/PrintServices\\lp59-2401.pdf",
+            "C:/PrintServices\\lp59-3302.pdf",
+            "C:/PrintServices\\lp59-3303.pdf",
+            "C:/PrintServices\\lp59-3702.pdf",
+            "C:/PrintServices\\lp59-4501.pdf",
+            "C:/PrintServices\\lp59-4502.pdf",
+            "C:/PrintServices\\lp59-4503.pdf",
+            "C:/PrintServices\\lp59-4701.pdf",
+            "C:/PrintServices\\lp59-4802.pdf",
+            "C:/PrintServices\\lp59-4803.pdf",
+            "C:/PrintServices\\tndc06-1201.pdf",
+            "C:/PrintServices\\tndc06-1202.pdf",
+            "C:/PrintServices\\tndc06-1203.pdf",
+            "C:/PrintServices\\tndc06-1207.pdf",
+            "C:/PrintServices\\tndc06-1401.pdf",
+            "C:/PrintServices\\tndc06-1402.pdf",
+            "C:/PrintServices\\tndc06-1501.pdf",
+            "C:/PrintServices\\tndc06-1701.pdf",
+            "C:/PrintServices\\tndc06-1801.pdf",
+            "C:/PrintServices\\tndc06-1802.pdf",
+            "C:/PrintServices\\tndc06-2001.pdf",
+            "C:/PrintServices\\tndc06-2401.pdf",
+            "C:/PrintServices\\tndc06-2402.pdf",
+            "C:/PrintServices\\tndc06-2701.pdf",
+            "C:/PrintServices\\tndc06-2704.pdf",
+            "C:/PrintServices\\tndc06-3103.pdf",
+            "C:/PrintServices\\tndc06-3106.pdf",
+            "C:/PrintServices\\tndc06-3107.pdf",
+            "C:/PrintServices\\tndc06-3302.pdf",
+            "C:/PrintServices\\tndc06-3303.pdf",
+            "C:/PrintServices\\tndc06-3401.pdf",
+            "C:/PrintServices\\tndc06-3402.pdf",
+            "C:/PrintServices\\tndc06-3601.pdf",
+            "C:/PrintServices\\tndc06-3602.pdf",
+            "C:/PrintServices\\tndc06-3603.pdf",
+            "C:/PrintServices\\tndc06-3701.pdf",
+            "C:/PrintServices\\tndc06-3702.pdf",
+            "C:/PrintServices\\tndc06-3703.pdf",
+            "C:/PrintServices\\tndc06-3705.pdf"
+         };
+    }
+}
