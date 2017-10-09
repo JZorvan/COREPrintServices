047640c9b3b7fefdd9bcfc3ac63e053cb3923039
JZorvan <jzorvan@gmail.com>
2017-10-09 12:15:43 -0500

    Added page and doc totals and the page counter function

---
+++ PrintServices/Counter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PrintServices
+{
+    class Counter
+    {
+        public static int totalPageCount = 0;
+        public static int totalDocCount = 0;
+        public static int AddToTotalCount(string file, int pageCount)
+        {
+            totalDocCount++;
+            if (FileInfo.TwoSidedFiles.Contains(file))
+            {
+                totalPageCount += (pageCount / 2);
+            }
+            else
+            {
+                totalPageCount += pageCount;
+            }
+            return totalPageCount;
+        }
+        public static int getNumberOfPages(string fileName)
+        {
+            using (StreamReader streamReader = new StreamReader(File.OpenRead(fileName)))
+            {
+                Regex regex = new Regex(@"/Type\s*/Page[^s]");
+                MatchCollection matches = regex.Matches(streamReader.ReadToEnd());
+                return matches.Count;
+            }
+        }
+    }
+}
