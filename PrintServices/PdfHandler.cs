33f08a9b6f4c442c9835ad0cbf43275e1a97bbc4
JZorvan <jzorvan@gmail.com>
2017-10-16 13:40:15 -0500

    Added functionality to merge pdfs

--- PrintServices/PdfHandler.cs
+++ PrintServices/PdfHandler.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace PrintServices
 {
@@ -12,18 +15,45 @@ namespace PrintServices
         public static void findDuplicates()
         {
             string[] pdfFiles = Directory.GetFiles("C:/PrintServices", "*.pdf");
-            foreach (string filename in FileInfo.allFiles)
+            List<string> duplicates = new List<string>();
+            foreach (string jobName in FileInfo.allFiles)
             {
-                var result = Array.FindAll(pdfFiles, s => s.Contains(filename));
+                var result = Array.FindAll(pdfFiles, s => s.Contains(jobName));
                 if (result.Count() > 1)
                 {
-                    Console.WriteLine(filename + " has " + result.Count() + " files that need to be combined.");
+                    duplicates = result.ToList();
+                    mergePdfs(duplicates, jobName, result.Count());
                 } 
             }
         }
-        public static void combinePdfs()
+        public static void mergePdfs(List<string> duplicateFiles, string jobName, int numDocs)
         {
+            string mergedFile = "C:/PrintServices\\" + jobName + ".pdf";
+            using (FileStream stream = new FileStream(mergedFile, FileMode.Create))
+            using (Document doc = new Document())
+            using (PdfCopy pdf = new PdfCopy(doc, stream))
+            {
+                doc.Open();
 
+                PdfReader reader = null;
+                PdfImportedPage page = null;
+
+                duplicateFiles.ForEach(file =>
+                {
+                    reader = new PdfReader(file);
+
+                    for (int i = 0; i < reader.NumberOfPages; i++)
+                    {
+                        page = pdf.GetImportedPage(reader, i + 1);
+                        pdf.AddPage(page);
+                    }
+
+                    pdf.FreeReader(reader);
+                    reader.Close();
+                    File.Delete(file);
+                });
+                Console.WriteLine("There were " + numDocs + " files for the " + jobName + " job that were merged into one file.");
+            }
         }
         public static void renameFiles()
         {
