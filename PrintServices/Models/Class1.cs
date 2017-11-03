05698888783942da10dd5ff6aa868b1f139766f2
Janelle Zorvan <AG20375@tn.gov>
2017-11-03 14:01:31 -0500

    Add Job Model

---
+++ PrintServices/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToneDownThatBackEnd.Models
+{
+    public class Job
+    {
+        [Key]
+        public int EntryId { get; set; }
+        public string FileName { get; set; }
+        public int PageCount { get; set; }
+        public string PrintQueue { get; set; }
+        public string Board { get; set; }
+        public string Disposition { get; set; }
+    }
+}
