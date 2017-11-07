17d9b1977b78816ef186fa54febb87d30755f92b
Janelle Zorvan <AG20375@tn.gov>
2017-11-07 15:27:59 -0600

    fixed namespace on model

--- PrintServices/Models/Job.cs
+++ PrintServices/Models/Job.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace ToneDownThatBackEnd.Models
+namespace PrintServices.Models
 {
     public class Job
     {
