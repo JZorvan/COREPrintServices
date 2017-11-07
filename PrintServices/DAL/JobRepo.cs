b63cb68cf81144736e4b84e9c0ed8d60fa099130
Janelle Zorvan <AG20375@tn.gov>
2017-11-07 12:17:48 -0600

    Import from spreadsheet method is isolating and reading the data needed

--- PrintServices/DAL/JobRepo.cs
+++ PrintServices/DAL/JobRepo.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToneDownThatBackEnd.Models;
+using Microsoft.Office.Interop.Excel;
 
 namespace PrintServices.DAL
 {
@@ -20,7 +22,41 @@ namespace PrintServices.DAL
         }
         public void ImportMasterSpreadsheet()
         {
-            // Create db from the master spreadsheet with zero page counts
+            Console.WriteLine("I'm trying to read the MasterList.");
+            Application excel = new Application();
+            //excel.DisplayAlerts = false;
+            Workbook workbook = excel.Workbooks.Open("C:/PrintServices\\TestMasterList.xlsx", ReadOnly: false, Editable: false);
+            Worksheet worksheet = workbook.Worksheets.Item[1] as Worksheet;
+            if (worksheet == null)
+                return;
+
+            Range fileNames = worksheet.Columns["A"];
+            Range pageCounts = worksheet.Columns["B"];
+            Range PrintQueues = worksheet.Columns["C"];
+            Range Boards = worksheet.Columns["D"];
+            Range Dispositions = worksheet.Columns["E"];
+            Range range = worksheet.UsedRange;
+            int totalColumns = worksheet.UsedRange.Columns.Count;
+            int totalRows = worksheet.UsedRange.Rows.Count;
+            Console.WriteLine(totalColumns);
+            Console.WriteLine(totalRows);
+
+            for (int i = 2; i <= totalRows; i++)
+            {
+                Console.WriteLine(range.Cells[i, 1].Value);
+                Console.WriteLine(range.Cells[i, 2].Value);
+                Console.WriteLine(range.Cells[i, 3].Value);
+                Console.WriteLine(range.Cells[i, 4].Value);
+                Console.WriteLine(range.Cells[i, 5].Value);
+
+            }
+
+
+            workbook.Close();
+            excel.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
         }
         public List<Job> GetJobs()
         {
