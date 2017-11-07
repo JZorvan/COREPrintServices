9f45a5767b3ef5728ce9a828a66ab75d7e4d6fa2
Janelle Zorvan <AG20375@tn.gov>
2017-11-07 15:29:59 -0600

    Repo populates from spreadsheet

--- PrintServices/Program.cs
+++ PrintServices/Program.cs
@@ -5,35 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using PrintServices.DAL;
+using PrintServices.Models;
 
 namespace PrintServices
 {
     class Program
     {
         static void Main(string[] args)
-        {   //Notes on making the path automatic for other users:
-            //string path = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
-            //string filepath = Path.GetFullPath(Path.Combine(path, ".."));
-            //Console.WriteLine(filepath);
-
-            FileInfo.remove3502AA();
-
-            List<Task> tasks = new List<Task>()
+        {
+            JobRepo jobRepo = new JobRepo();
+            //jobRepo.ImportMasterSpreadsheet();
+            List<Job> jobs = jobRepo.GetJobs();
+            foreach (Job job in jobs)
             {
-                new Task(() => WordHandler.convertToPdf()),
-                new Task(() => PdfHandler.findDuplicates()),
-                new Task(() => PdfHandler.renameFiles()),
-                new Task(() => ExcelHandler.populateSpreadsheet()),
-                new Task(() => ConsoleHandler.printToConsole())
-            };
-
-            foreach (Task t in tasks)
-            {
-                t.Start();
-                t.Wait();
+                Console.WriteLine(job.FileName);
             }
 
-            Console.ReadKey();
+
+
+
+                //Notes on making the path automatic for other users:
+                //string path = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+                //string filepath = Path.GetFullPath(Path.Combine(path, ".."));
+                //Console.WriteLine(filepath);
+
+                //FileInfo.remove3502AA();
+
+                //List<Task> tasks = new List<Task>()
+                //{
+                //    new Task(() => WordHandler.convertToPdf()),
+                //    new Task(() => PdfHandler.findDuplicates()),
+                //    new Task(() => PdfHandler.renameFiles()),
+                //    new Task(() => ExcelHandler.populateSpreadsheet()),
+                //    new Task(() => ConsoleHandler.printToConsole())
+                //};
+
+                //foreach (Task t in tasks)
+                //{
+                //    t.Start();
+                //    t.Wait();
+                //}
+
+                Console.ReadKey();
+
         }
     }
 }
