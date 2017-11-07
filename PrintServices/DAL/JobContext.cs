29f3429eacb54a4e0ad54ec5a90f53ce27b62301
Janelle Zorvan <AG20375@tn.gov>
2017-11-07 11:23:20 -0600

    Added blank method for importing the spreadsheet

--- PrintServices/DAL/JobContext.cs
+++ PrintServices/DAL/JobContext.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToneDownThatBackEnd.Models;
 
 namespace PrintServices.DAL
 {
-    class JobContext
+    public class JobContext : DbContext
     {
+        public virtual DbSet<Job> Jobs { get; set; }
     }
 }
--- PrintServices/DAL/JobRepo.cs
+++ PrintServices/DAL/JobRepo.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToneDownThatBackEnd.Models;
 
 namespace PrintServices.DAL
 {
-    class JobRepo
+    public class JobRepo
     {
+        public JobContext Context { get; set; }
+        public JobRepo()
+        {
+            Context = new JobContext();
+        }
+        public JobRepo(JobContext _context)
+        {
+            Context = _context;
+        }
+        public void ImportMasterSpreadsheet()
+        {
+            // Create db from the master spreadsheet with zero page counts
+        }
+        public List<Job> GetJobs()
+        {
+            int i = 1;
+            return Context.Jobs.ToList();
+        }
     }
 }
