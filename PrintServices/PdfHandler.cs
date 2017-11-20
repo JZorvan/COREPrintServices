b6eee5bb4691666f832e929e73c46412eb07ddb0
Janelle Zorvan <AG20375@tn.gov>
2017-11-20 10:48:43 -0600

    added a method to get pdf files from the folder

--- PrintServices/PdfHandler.cs
+++ PrintServices/PdfHandler.cs
@@ -14,21 +14,21 @@ namespace PrintServices
     class PdfHandler
     {
 
-        public static void findDuplicates(List<Job> joblist)
+        public static void handleDuplicates(List<Job> joblist)
         {
             string[] pdfFiles = Directory.GetFiles("C:/PrintServices", "*.pdf");
             List<string> duplicates = new List<string>();
             foreach (Job job in joblist)
             {
-                var result = Array.FindAll(pdfFiles, s => s.Contains(filenameTrimmer(job.FileName)));
+                var result = Array.FindAll(pdfFiles, s => s.Contains(filenameTrimmer(job)));
                 if (result.Count() > 1)
                 {
                     duplicates = result.ToList();
-                    mergePdfs(duplicates, filenameTrimmer(job.FileName), result.Count());
-                } 
+                    mergePdfs(duplicates, filenameTrimmer(job));
+                }
             }
         }
-        public static void mergePdfs(List<string> duplicateFiles, string jobName, int numDocs)
+        public static void mergePdfs(List<string> duplicates, string jobName)
         {
             string mergedFile = "C:/PrintServices\\" + jobName + ".pdf";
             using (FileStream stream = new FileStream(mergedFile, FileMode.Create))
@@ -40,7 +40,7 @@ namespace PrintServices
                 PdfReader reader = null;
                 PdfImportedPage page = null;
 
-                duplicateFiles.ForEach(file =>
+                duplicates.ForEach(file =>
                 {
                     reader = new PdfReader(file);
 
@@ -54,55 +54,80 @@ namespace PrintServices
                     reader.Close();
                     File.Delete(file);
                 });
-                Console.WriteLine("There were " + numDocs + " files for the " + jobName + " job that were merged into one file.");
             }
         }
-        public static void renameFiles()
+        public static void renameFile(string file, string newFilename)
         {
+                File.Move(file, newFilename);
+        }
+        public static List<string> getFiles()
+        {
+            List<string> files = new List<string>();
             foreach (string file in Directory.EnumerateFiles("C:/PrintServices", "*.pdf"))
             {
-                File.Move(file, getNewFilename(file));
+                files.Add(file);
             }
+            return files;
         }
-        public static string getNewFilename(string file)
+        public static void handleRenaming(List<Job> jobs)
         {
-            if (file.Contains("lp32-15C"))
+            List<string> files = getFiles();
+            var result = "";
+            foreach (Job job in jobs)
             {
-                return file.Substring(0, 25) + ".pdf";
-            }
-            else if (file.Contains("lp32-19C"))
-            {
-                return file.Substring(0, 25) + ".pdf";
-            }
-            else if (file.Contains("lp59-15C"))
-            {
-                return file.Substring(0, 25) + ".pdf";
-            }
-            else if (file.Contains("lp59-1601B"))
-            {
-                return file.Substring(0, 27) + ".pdf";
-            }
-            else if (file.Contains("lp59-3502B"))
-            {
-                return file.Substring(0, 27) + ".pdf";
-            }
-            else if (file.Contains("25CAN001"))
-            {
-                return "C:/PrintServices\\25CAN001.pdf";
-            }
-            else if (file.Contains("25CAA001"))
-            {
-                return "C:/PrintServices\\25CAA001.pdf";
-            }
-            else if (file.Contains("tndc06"))
-            {
-                return file.Substring(0, 28) + ".pdf";
-            }
-            else
-            {
-                return file.Substring(0, 26) + ".pdf";
+                result = files.Find(f => f.Contains(filenameTrimmer(job)));
+                if (result != null)
+                {
+                    renameFile(result, "C:/PrintServices\\" + job.FileName);
+                } else { continue; }
             }
         }
+        //public static void renameFiles()
+        //{
+        //    foreach (string file in Directory.EnumerateFiles("C:/PrintServices", "*.pdf"))
+        //    {
+        //        File.Move(file, getNewFilename(file));
+        //    }
+        //}
+        //public static string getNewFilename(string file)
+        //{
+        //    if (file.Contains("lp32-15C"))
+        //    {
+        //        return file.Substring(0, 25) + ".pdf";
+        //    }
+        //    else if (file.Contains("lp32-19C"))
+        //    {
+        //        return file.Substring(0, 25) + ".pdf";
+        //    }
+        //    else if (file.Contains("lp59-15C"))
+        //    {
+        //        return file.Substring(0, 25) + ".pdf";
+        //    }
+        //    else if (file.Contains("lp59-1601B"))
+        //    {
+        //        return file.Substring(0, 27) + ".pdf";
+        //    }
+        //    else if (file.Contains("lp59-3502B"))
+        //    {
+        //        return file.Substring(0, 27) + ".pdf";
+        //    }
+        //    else if (file.Contains("25CAN001"))
+        //    {
+        //        return "C:/PrintServices\\25CAN001.pdf";
+        //    }
+        //    else if (file.Contains("25CAA001"))
+        //    {
+        //        return "C:/PrintServices\\25CAA001.pdf";
+        //    }
+        //    else if (file.Contains("tndc06"))
+        //    {
+        //        return file.Substring(0, 28) + ".pdf";
+        //    }
+        //    else
+        //    {
+        //        return file.Substring(0, 26) + ".pdf";
+        //    }
+        //}
         public static string filenameTrimmer(string file)
         {
             char[] pdfString = { '.', 'p', 'd', 'f' };
