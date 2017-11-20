4e67480ebecfd9f3ab3052535958f59888254c50
Janelle Zorvan <AG20375@tn.gov>
2017-11-20 10:50:11 -0600

    added a method to rename the pdf files and implemented in program

--- PrintServices/Program.cs
+++ PrintServices/Program.cs
@@ -18,22 +18,25 @@ namespace PrintServices
             JobRepo db = new JobRepo();
             Task ImportMasterSpreadsheet = new Task(() => db.ImportMasterSpreadsheet());
             Task ConvertToPdf = new Task(() => WordHandler.convertToPdf());
-            Task RenameFiles = new Task(() => PdfHandler.renameFiles());
+            
             Task PopulateSpreadsheet = new Task(() => ExcelHandler.populateSpreadsheet());
             Task PrintToConsole = new Task(() => ConsoleHandler.printToConsole());
 
             ImportMasterSpreadsheet.Start();
-                ImportMasterSpreadsheet.Wait();
+            ImportMasterSpreadsheet.Wait();
             List<Job> jobs = db.GetJobs();
-            FileInfo.removeFilesToDelete(); 
+            FileInfo.removeFilesToDelete();
             ConvertToPdf.Start();
-                ConvertToPdf.Wait();
-            Task FindDuplicates = new Task(() => PdfHandler.findDuplicates(jobs));
-            FindDuplicates.Start();
-                FindDuplicates.Wait();
+            ConvertToPdf.Wait();
+            Task handleDuplicates = new Task(() => PdfHandler.handleDuplicates(jobs));
+            handleDuplicates.Start();
+            handleDuplicates.Wait();
+            Task RenameFiles = new Task(() => PdfHandler.handleRenaming(jobs));
+            RenameFiles.Start();
+            RenameFiles.Wait();
 
 
-            //db.ClearRepository();
+            db.ClearRepository();
 
             //Notes on making the path automatic for other users:
             //string path = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
