9a627de117b9d45d7cc86d555d841a32fe9d7958
JZorvan <jzorvan@gmail.com>
2017-12-07 09:21:36 -0600

    added potential message

--- PrintServices/ConsoleHandler.cs
+++ PrintServices/ConsoleHandler.cs
@@ -8,5 +8,19 @@ namespace PrintServices
 {
     class ConsoleHandler
     {
+        public static void Print(string input)
+        {
+            switch (input)
+            {
+                case "greeting":
+                    Console.WriteLine("Welcome to the Print and Mail Services Application.");
+                    break;
+                case "no files":
+                    Console.WriteLine("There are no files in your folder.");
+                    Console.WriteLine("Please copy today's files from JBoss to your PrintServices folder.");
+                    break;
+                
+            }
+        }
     }
 }
